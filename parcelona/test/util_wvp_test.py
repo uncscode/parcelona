6606e15a5a2c5f4aafffa5d695eb014ea06d94b3
ngam <67342040+ngam@users.noreply.github.com>
2022-10-08 20:57:05 +0000

    lint

--- parcelona/test/util_wvp_test.py
+++ parcelona/test/util_wvp_test.py
@@ -1,4 +1,4 @@
-""" test: parcelona.util.water_vapor_pressure """
+""" test: parcelona.util.wvpsat """
 
 from particula import u
 from parcelona.util.wvpsat import buck_wvpsat
@@ -9,6 +9,7 @@ def test_buck_wvp():
 
     temperature1 = (300 * u.K).to("degC")
     temperature2 = (350 * u.K).to("degC")
+
     assert buck_wvpsat(temperature1).u == u.hPa
     assert buck_wvpsat(temperature1.to_base_units()).u == u.hPa
     assert buck_wvpsat(temperature1) < buck_wvpsat(temperature2)
