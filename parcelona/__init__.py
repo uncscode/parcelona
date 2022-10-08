83cd1c63adfc131a1aa5a2165a99bb0a76f8f4ac
ngam <67342040+ngam@users.noreply.github.com>
2022-10-07 22:55:59 -0400

    0.0.0 (#4)

---
+++ parcelona/__init__.py
@@ -0,0 +1,3 @@
+""" parcelona """
+
+__version__ = "0.0.0"
---
+++ parcelona/test/main_test.py
@@ -0,0 +1,8 @@
+""" test: parcelona.main """
+
+from parcelona import main
+
+
+def test_main():
+    """ test: parcelona.main """
+    assert main.__something__ == "something"
