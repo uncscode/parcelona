75f096cdcb6649a21e5543c5444388ffd1ec3069
ngam <67342040+ngam@users.noreply.github.com>
2022-10-08 20:30:35 +0000

    move to np.exp

--- parcelona/util/wvp.py
+++ parcelona/util/wvp.py
@@ -1,6 +1,6 @@
 """ parcelona.util.water_vapor_pressure """
 
-import math
+import numpy as np
 
 from particula import u
 from particula.util.input_handling import in_temperature
@@ -13,11 +13,8 @@ def buck_wvp(temperature):
 
     temp = in_temperature(temperature).m_as("degC")
 
-    if temp >= 0.0:
-        return 6.1121 * math.exp(
-            (18.678-temp/234.5)*(temp/(257.14+temp))
-        )*u.hPa
-
-    return 6.1115 * math.exp(
+    return 6.1115 * np.exp(
         (23.036-temp/333.7)*(temp/(279.82+temp))
-    )*u.hPa
+    )*u.hPa * (temp < 0.0) + 6.1121 * np.exp(
+        (18.678-temp/234.5)*(temp/(257.14+temp))
+    )*u.hPa * (temp >= 0.0)
