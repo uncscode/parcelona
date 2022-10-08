6e60fdc04d72a9e3973aaa72601e2314fd111305
ngam <67342040+ngam@users.noreply.github.com>
2022-10-08 11:15:54 -0400

    lint

--- parcelona/parenv.py
+++ parcelona/parenv.py
@@ -2,11 +2,11 @@
 
 from particula.environment import Environment
 
+
 class ParEnv(Environment):
     """ ParEnv """
     def __init__(self):
         super().__init__()
-        self.add('parcelona', 'parcelona')
 
     def __str__(self):
         return 'ParEnv'
