2ca5a26c0617715bab34121141b23d1db2ead4ec
ngam <67342040+ngam@users.noreply.github.com>
2022-10-08 19:59:52 +0000

    lint

--- parcelona/test/util_wvp_test.py
+++ parcelona/test/util_wvp_test.py
@@ -1,11 +1,13 @@
 """ test: parcelona.util.water_vapor_pressure """
 
 from particula import u
-from parcelona.util.water_vapor_pressure import buck_wvp
+from parcelona.util.wvp import buck_wvp
 
 
 def test_buck_wvp():
     """ test: buck_wvp """
+
     temperature = (300 * u.K).to("degC")
+
     assert buck_wvp(temperature).u == u.hPa
     assert buck_wvp(temperature.to_base_units()).u == u.hPa
--- parcelona/util/wvp.py
+++ parcelona/util/wvp.py
@@ -8,11 +8,14 @@ from particula.util.input_handling import in_temperature
 
 def buck_wvp(temperature, phase="liquid"):
     """ Buck equation for water vapor pressure """
+
     temp = in_temperature(temperature).m_as("degC")
+
     if phase == "liquid" or temp >= 0.0:
         return 6.1121 * math.exp(
             (18.678-temp/234.5)*(temp/(257.14+temp))
         )*u.hPa
+
     return 6.1115 * math.exp(
         (23.036-temp/333.7)*(temp/(279.82+temp))
     )*u.hPa
