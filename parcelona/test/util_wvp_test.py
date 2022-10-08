582d0169c9eb9a3a6c87f1ef1e1637df47c9a3bf
ngam <67342040+ngam@users.noreply.github.com>
2022-10-08 20:05:28 +0000

    more testing

--- parcelona/test/util_wvp_test.py
+++ parcelona/test/util_wvp_test.py
@@ -7,7 +7,8 @@ from parcelona.util.wvp import buck_wvp
 def test_buck_wvp():
     """ test: buck_wvp """
 
-    temperature = (300 * u.K).to("degC")
-
-    assert buck_wvp(temperature).u == u.hPa
-    assert buck_wvp(temperature.to_base_units()).u == u.hPa
+    temperature1 = (300 * u.K).to("degC")
+    temperature2 = (350 * u.K).to("degC")
+    assert buck_wvp(temperature1).u == u.hPa
+    assert buck_wvp(temperature1.to_base_units()).u == u.hPa
+    assert buck_wvp(temperature1) < buck_wvp(temperature2)
