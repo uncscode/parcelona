952b40ae101a347e6e2c0022ec73b893b052ef09
ngam <67342040+ngam@users.noreply.github.com>
2022-10-09 15:39:02 -0400

    delete kwargs

--- parcelona/util/kappa_kohler.py
+++ parcelona/util/kappa_kohler.py
@@ -14,7 +14,6 @@ def particle_phase_h2o_activity(
     dry_radius,
     wet_radius,
     kappa_ccn,
-    **kwargs
 ):
     """ kappa equilibrium water activity over a material.
 
@@ -82,7 +81,6 @@ def particle_h2o_activation_radius(
     temperature,
     dry_radius,
     kappa_ccn,
-    **kwargs
 ):
     """ activation radius of a particle.
 
